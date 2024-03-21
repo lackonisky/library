66f846adb5c0a81e90bce9726b9f6c1b023cab8a
lackonisky <ben.baker33@outlook.com>
2024-03-21 23:39:30 +0000

    sjndjs

--- import sqlite3.py
+++ import sqlite3.py
@@ -1,32 +1,57 @@
 #import the sqlite3 library so it can be used to interact with the database
 import sqlite3
-# connects to the database and creates one if one is not already present
-connection = sqlite3.connect("library5.db")
-# a database cursor is created so it can be used to execute SQL commands
-cursor = connection.cursor()
-# the cursor is used to execute the create table command within the database to create the books table
-cursor.execute("""CREATE TABLE "Books" (
-	"BOOKNO"	INTEGER,
-	"ISBN"	TEXT,
-	"AUTHOR"	TEXT,
-	"TITLE"	TEXT,
-	"DATEPUBLISHED"	TEXT,
-	"FORMAT"	TEXT,
-	"STOCK"	INTEGER,
-	"DESCRIPTION"	TEXT,
-	PRIMARY KEY("BOOKNO" AUTOINCREMENT)
-);"""
-)
-# the cursor is used to execute the create table command within the database to create the users table
-cursor.execute("""CREATE TABLE "Users" (
-	"IDNO"	INTEGER,
-	"USERNAME"	TEXT,
-	"PASS"	TEXT,
-	"ACCESS"	INTEGER,
-	PRIMARY KEY("IDNO" AUTOINCREMENT)
-);"""
-)
-cursor.execute("""INSERT INTO Users (USERNAME, PASS, ACCESS)
-VALUES ('admin', 'fb001dfcffd1c899f3297871406242f097aecf1a5342ccf3ebcd116146188e4b', 1);
-""")
-connection.commit()
+import os
+import hashlib 
+import pyfiglet
+
+def initialize():
+	if os.path.exists("./library2.db") == True:
+		print("Database Found")
+		login()
+	else:
+		initialization = pyfiglet.figlet_format("System Initialization")
+		print(initialization)
+		create_database()
+
+def password_hashing(password): # function to hash the password and return the hash
+    password = password.encode('utf-8')
+    password = hashlib.sha3_256(password).hexdigest()
+    return password
+
+def create_database():
+	# connects to the database and creates one if one is not already present
+	connection = sqlite3.connect("library2.db")
+	# a database cursor is created so it can be used to execute SQL commands
+	cursor = connection.cursor()
+	# the cursor is used to execute the create table command within the database to create the books table
+	cursor.execute("""CREATE TABLE "Books" (
+		"BOOKNO"	INTEGER,
+		"ISBN"	TEXT,
+		"AUTHOR"	TEXT,
+		"TITLE"	TEXT,
+		"DATEPUBLISHED"	TEXT,
+		"FORMAT"	TEXT,
+		"STOCK"	INTEGER,
+		"DESCRIPTION"	TEXT,
+		PRIMARY KEY("BOOKNO" AUTOINCREMENT)
+	);"""
+	)
+	# the cursor is used to execute the create table command within the database to create the users table
+	cursor.execute("""CREATE TABLE "Users" (
+		"IDNO"	INTEGER,
+		"USERNAME"	TEXT,
+		"PASS"	TEXT,
+		"ACCESS"	INTEGER,
+		PRIMARY KEY("IDNO" AUTOINCREMENT)
+	);"""
+	)
+	connection.commit()
+	username = input("""Enter the username you'd like to use for the base admin account""")
+	password = input("""Enter the password you'd like to use for the base admin account""")
+	password_hashing(password)
+	cursor.execute("""INSERT INTO Users (USERNAME, PASS, ACCESS)
+VALUES (?, ?, 1);""", (username, password,))
+	connection.commit()
+	initialize()
+
+initialize()
--- main.py
+++ main.py
@@ -3,24 +3,81 @@ import hashlib
 import pyfiglet
 from simple_term_menu import TerminalMenu
 import time
-initialising = pyfiglet.figlet_format("Initialising")
-print(initialising)
-connection = sqlite3.connect("library.db")
-cursor = connection.cursor()
-welcome = pyfiglet.figlet_format("Welcome to Benne's Library")
-# time.sleep(1.5)
-print(welcome)
+import os
+
+def initialize():
+	if os.path.exists("./library.db") == True:
+		print(pyfiglet.figlet_format("Found"))
+		database_connection()
+	else:
+		initialization = pyfiglet.figlet_format("System Initialization")
+		print(initialization)
+		create_database()
+
+def create_database():
+	# connects to the database and creates one if one is not already present
+	connection = sqlite3.connect("library.db")
+	# a database cursor is created so it can be used to execute SQL commands
+	cursor = connection.cursor()
+	# the cursor is used to execute the create table command within the database to create the books table
+	cursor.execute("""CREATE TABLE "Books" (
+		"BOOKNO"	INTEGER,
+		"ISBN"	TEXT,
+		"AUTHOR"	TEXT,
+		"TITLE"	TEXT,
+		"DATEPUBLISHED"	TEXT,
+		"FORMAT"	TEXT,
+		"STOCK"	INTEGER,
+		"DESCRIPTION"	TEXT,
+		PRIMARY KEY("BOOKNO" AUTOINCREMENT)
+	);"""
+	)
+	# the cursor is used to execute the create table command within the database to create the users table
+	cursor.execute("""CREATE TABLE "Users" (
+		"IDNO"	INTEGER,
+		"USERNAME"	TEXT,
+		"PASS"	TEXT,
+		"ACCESS"	INTEGER,
+		PRIMARY KEY("IDNO" AUTOINCREMENT)
+	);"""
+	)
+	connection.commit()
+	username = input("""Enter the username you'd like to use for the base admin account""")
+	password = input("""Enter the password you'd like to use for the base admin account""")
+	password_hashing(password)
+	cursor.execute("""INSERT INTO Users (USERNAME, PASS, ACCESS)
+VALUES (?, ?, 1);""", (username, password,))
+	connection.commit()
+	initialize()
+
+def database_connection():
+    initializing = pyfiglet.figlet_format("Initialising")
+    print(initializing)
+    global connection
+    connection = sqlite3.connect("library.db")
+    global cursor
+    cursor = connection.cursor()
+    welcome = pyfiglet.figlet_format("Welcome to Benne's Library")
+    # time.sleep(1.5)
+    print(welcome)
+    login()
 
 def simple_menu():
     print("Main Menu")
-    choices = ["Search Books", "View book list", "Account Management"]
+    choices = ["Search Books", "View book list"]
     menu = TerminalMenu(choices)
     output = menu.show()
     print("You selected ", choices[output])
+    if output == 0:
+        view_book()
+    elif output == 1:
+        item_list()
+    else:
+        menu_check()
 
 def admin_menu():
     print("Admin Menu")
-    choices = ["Search Books", "View Item list", "Account Management", "User Management", "Inventory Management"]
+    choices = ["Search Books", "View Item list", "User Management", "Inventory Management"]
     menu = TerminalMenu(choices)
     output = menu.show()
     print("You selected", choices[output], """ 
@@ -30,10 +87,8 @@ def admin_menu():
     elif output == 1:
         item_list()
     elif output == 2:
-        account_manager()
-    elif output == 3:
         user_manager()
-    elif output == 4:
+    elif output == 3:
         inventory()
 
 def inventory():
@@ -61,11 +116,11 @@ def password_hashing(password): # function to hash the password and return the h
     return password
 
 def login():
-    # time.sleep(2)
+    time.sleep(0.5)
     print("""Login Portal
           """)
-    username = input("Please input your username")
-    password = input("Please enter your password")
+    username = input("Please input your username: ")
+    password = input("Please enter your password: ")
     password = password_hashing(password)
     print(password)
     cursor.execute("SELECT PASS from Users WHERE USERNAME = ?", (username,))
@@ -115,34 +170,49 @@ def view_book():
     print_item(bookinfo)
 
 def book_number():
-    num = input("input the book number ")
-    cursor.execute("SELECT * from Books WHERE BOOKNO = ?",(num,))
-    bookinfo = cursor.fetchall()
-    bookinfo = bookinfo[0]
-    return bookinfo
+    try:
+        num = input("input the book number ")
+        cursor.execute("SELECT * from Books WHERE BOOKNO = ?",(num,))
+        bookinfo = cursor.fetchall()
+        bookinfo = bookinfo[0]
+        return bookinfo
+    except:
+        print("Book not found or invalid input")
+        menu_check()
 
 def ISBN_search():###################
-    isbn = input("input the book number ")
-    cursor.execute("SELECT * from Books WHERE ISBN = ?",(isbn,))
-    bookinfo = cursor.fetchall()
-    bookinfo = bookinfo[0]
-    return bookinfo
+    try:
+        isbn = input("input the book number ")
+        cursor.execute("SELECT * from Books WHERE ISBN = ?",(isbn,))
+        bookinfo = cursor.fetchall()
+        bookinfo = bookinfo[0]
+        return bookinfo
+    except:
+        print("Book not found or invalid input")
+        menu_check()
 
 def Name_search():#####################
-    name = input("input the book number ")
-    name = "%" + name + "%"
-    cursor.execute("SELECT * from Books WHERE TITLE LIKE ?",(name,))
-    bookinfo = cursor.fetchall()
-    bookinfo = bookinfo[0]
-    return bookinfo
-
+    try:
+        name = input("input the book number ")
+        name = "%" + name + "%"
+        cursor.execute("SELECT * from Books WHERE TITLE LIKE ?",(name,))
+        bookinfo = cursor.fetchall()
+        bookinfo = bookinfo[0]
+        return bookinfo
+    except:
+        print("Book not found or invalid input")
+        menu_check()
 def Author_search(): ######################
-    author = input("input the book number ")
-    author = "%" + author + "%"
-    cursor.execute("SELECT * from Books WHERE AUTHOR LIKE ?",(author,))
-    bookinfo = cursor.fetchall()
-    bookinfo = bookinfo[0]
-    return bookinfo
+    try:
+        author = input("input the Authors name ")
+        author = "%" + author + "%"
+        cursor.execute("SELECT * from Books WHERE AUTHOR LIKE ?",(author,))
+        bookinfo = cursor.fetchall()
+        bookinfo = bookinfo[0]
+        return bookinfo
+    except:
+        print("Book not found or invalid input")
+        menu_check()
 
 def user_manager():
     print("Account Manager")
@@ -167,7 +237,10 @@ def new_item():
     Author = input("Enter the Author of the item you'd like to add ")
     datePublished = input("Enter the Publishing date of the item you'd like to add in the format DD/MM/YYYY ")
     itemFormat = input("Enter the Format of the item you'd like to add ")
-    stockLevel = int(input("Enter the stock level of the item you'd like to add "))
+    while True:
+        stockLevel = input("Enter the stock level of the item you'd like to add ")
+        if stockLevel.isnumeric():
+            break
     itemDescription = input("Enter a description of the item you'd like to add")
     adding = """Title: {}
 ISBN: {}
@@ -216,16 +289,57 @@ Item Description: {}
           """)
     menu_check()
 
-def account_manager():
-    username = input("Enter the username of the account you'd like to manage")
 
 
 def edit_item():
-    menu_check()
+    while True:
+        bookNum = input("Enter the stock level of the item you'd like to add ")
+        if bookNum.isnumeric():
+            break
+    itemName = input("Enter the new Title of the item you'd like to change ")
+    ISBN = input("Enter the new ISBN of the item you'd like to change ")
+    Author = input("Enter the new Author of the item you'd like to change ")
+    datePublished = input("Enter the new Publishing date of the item you'd like to change in the format DD/MM/YYYY ")
+    itemFormat = input("Enter the new Format of the item you'd like to change ")
+    while True:
+        stockLevel = input("Enter the stock level of the item you'd like to add ")
+        if stockLevel.isnumeric():
+            break
+    itemDescription = input("Enter a new description for the item you'd like to change")
+    changing = """Title: {}
+ISBN: {}
+Author: {}
+Date Published: {}
+Item Format: {}
+Stock Level: {}
+Item Description: {}
+    """.format(itemName, ISBN, Author, datePublished, itemFormat, stockLevel, itemDescription)
+    print(changing)
+    check = ["Yes", "No"]
+    menu = TerminalMenu(check)
+    output = menu.show()
+    if output == 0:
+        cursor.execute("""UPDATE Books
+SET ISBN = ? ,
+    AUTHOR = ? ,
+    TITLE = ? ,
+    DATEPUBLISHED = ? ,
+    FORMAT = ? ,
+    STOCK = ? ,
+    DESCRIPTION = ?
+WHERE BOOKNO = ?;
+""", (ISBN, Author, itemName, datePublished, itemFormat, stockLevel, itemDescription, bookNum))
+        connection.commit()
+        print("succesful")
+        inventory()
+    else:
+        menu_check()
 
 def delete_account():
+    cursor.execute("""DELETE FROM BOOKS
+WHERE IDNO = ?""", (id,))
+    connection.commit()
     menu_check()
-
 
 def username_change():
     if level == 1:
@@ -267,13 +381,11 @@ def account_creation():
             break
     passw = password_hashing(passw)
     cursor.execute("""INSERT INTO Users (USERNAME, PASS, ACCESS)
+                   
 VALUES(?, ?, ?);""", (uName, passw, 0,))
     connection.commit()
     print("Account Created")
     menu_check()
 
 
-
-
-
-login()
+initialize()
